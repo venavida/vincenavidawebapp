b3e5515adaf40da142d554b68e1018ec010716fa
Vincenzo Roberto Navida <52182512+venavida@users.noreply.github.com>
2019-09-26 14:36:29 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.10.7, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.10.7, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/myface-122x122.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  <title>USERDES</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+  <section class="menu cid-rBEmnS1NH7" once="menu" id="menu1-1u">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="Home.html">
+                         <img src="assets/images/myface-122x122.png" alt="Mobirise" title="" style="height: 3.8rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="Home.html"><p><br></p><p>&nbsp; &nbsp; HOME</p></a></span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item">
+                    <a class="nav-link link text-white display-7" href="https://www.facebook.com/VinceNavidaLoca" target="_blank"><span class="socicon socicon-facebook mbr-iconfont mbr-iconfont-btn"></span>
+                        </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link text-white display-7" href="https://www.instagram.com/vincenavidaloca/" target="_blank"><span class="socicon socicon-instagram mbr-iconfont mbr-iconfont-btn" style="font-size: 25px;"></span>
+                        </a>
+                </li><li class="nav-item"><a class="nav-link link text-white display-7" href="https://soundcloud.com/vince-navida" target="_blank"><span class="socicon socicon-soundcloud mbr-iconfont mbr-iconfont-btn"></span>
+                        </a></li></ul>
+            
+        </div>
+    </nav>
+</section>
+
+<section class="engine"><a href="https://mobirise.info/x">free css templates</a></section><section class="cid-rBtl6fwfJc mbr-fullscreen mbr-parallax-background" id="header2-p">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.8; background-color: rgb(35, 35, 35);"></div>
+
+    <div class="container align-center">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-2">
+                    VINCENZO ROBERTO NAVIDA</h1>
+                <h3 class="mbr-section-subtitle align-center mbr-light pb-3 mbr-fonts-style display-2">
+                    21 | STUDENT | MUSICIAN</h3>
+                <p class="mbr-text pb-3 mbr-fonts-style display-5"></p>
+                <div class="mbr-section-btn"><a class="btn btn-md btn-secondary-outline display-4" href="Activities.html">ACTIVITIES</a> <a class="btn btn-md btn-secondary-outline display-4" href="Projects.html">PROJECTS</a> <a class="btn btn-md btn-secondary-outline display-4" href="Reflections.html">REFLECTIONS</a> <a class="btn btn-md btn-secondary-outline display-4" href="Quizzes.html">QUIZZES</a> <a class="btn btn-md btn-secondary-outline display-4" href="Certificates.html">CERTIFICATES</a></div>
+            </div>
+        </div>
+    </div>
+    
+</section>
+
+<section class="cid-rBEiP1S7i4 mbr-reveal" id="footer1-1t">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row content text-white">
+            <div class="col-12 col-md-3">
+                <div class="media-wrap">
+                    <a href="Home.html">
+                        <img src="assets/images/myface-192x192.png" alt="Mobirise" title="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Address
+                </h5>
+                <p class="mbr-text">743 Userdes St.&nbsp;<br>Your Imagination, Paranaque City</p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Contacts
+                </h5>
+                <p class="mbr-text">
+                    Email: vince2598@gmail.com<br>Phone: +63 995 187 5913<br><br></p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3"></h5>
+                <p class="mbr-text"></p>
+            </div>
+        </div>
+        
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/dropdown/js/nav-dropdown.js"></script>
+  <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  
+ <div id="scrollToTop" class="scrollToTop mbr-arrow-up"><a style="text-align: center;"><i class="mbr-arrow-up-icon mbr-arrow-up-icon-cm cm-icon cm-icon-smallarrow-up"></i></a></div>
+  </body>
+</html>
